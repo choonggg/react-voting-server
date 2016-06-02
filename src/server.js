0f04e25591e74e1020a7f5d4c39525da3067bdce
Chong Hui <iamchoonggg@gmail.com>
2016-06-02 18:15:15 +0800

    fix issue and added store

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export function startServer(store) {
   const io = Server(8090);
+
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
+  });
+
+  console.log('Is successfully port: 8090')
 }
